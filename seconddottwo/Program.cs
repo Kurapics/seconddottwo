b47c994edb6e0fe7b13aec127985eddb2563bc16
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:09:40 +0200

    Добавьте файлы проекта.

---
+++ seconddottwo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите сторону 1");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите сторону 2");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите сторону 3");
+double z = double.Parse(Console.ReadLine());
+
+if (x == y && x == z && y == z) Console.WriteLine("Треугольник равносторонний");
+else Console.WriteLine("Треугольник не равносторонний");
